68de089f263a90b5fa5be776544ceef071175eed
iluta.smite <smitite12@inbox.lv>
2021-02-20 10:14:45 +0200

    masa

---
+++ 9.uzdevums.py
@@ -0,0 +1,4 @@
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+print("Your body mass index is: ", round(weight / (height * height), 2))
+
